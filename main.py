e072e49cc786a9a1852d776a737c7ca1a52df79c
MakCimka <poduskapuska@gmail.com>
2024-10-08 12:26:58 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-#Вводим числа
-number_input=int(input("Введите значение всех четных чисел от 0 до n")) 
-#генератор списка 
-print([i for i in range(0,number_input,2)]) 
- 
+number_input=int(input("Введите значение всех четных чисел от 0 до n")) #Вводим числа
+print([i for i in range(0,number_input,2)]) #генерируем список 
